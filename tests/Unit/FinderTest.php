6a8d78252c1693b16c0c498a2a2fff0df92e4513
kubotak-is <kenjiro.kubota@gmail.com>
2021-12-23 23:35:23 +0900

    fix: line delete bug

--- tests/Unit/FinderTest.php
+++ tests/Unit/FinderTest.php
@@ -14,8 +14,8 @@ class FinderTest extends \PHPUnit\Framework\TestCase
 
         $flagList = $finder->getFlagList();
         self::assertEquals('flag_a', $flagList->offsetGet('flag_a')->get());
-        self::assertEquals(7, $flagList->offsetGet('flag_a')->count());
-        self::assertEquals('flag_a (7)', $flagList->offsetGet('flag_a'));
+        self::assertEquals(12, $flagList->offsetGet('flag_a')->count());
+        self::assertEquals('flag_a (12)', $flagList->offsetGet('flag_a'));
         self::assertEquals('flag_b', $flagList->offsetGet('flag_b')->get());
         self::assertEquals(3, $flagList->offsetGet('flag_b')->count());
         self::assertEquals('error-flag', $flagList->offsetGet('error-flag')->get());
